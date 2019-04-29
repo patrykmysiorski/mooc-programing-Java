eb2b06111c86b5cae59dc032eb6c9d9950b1f1a5
patryk.mysiorski <patryk.mysiorski@gmail.com>
2019-04-29 18:13:24 +0200

    92.1

---
+++ week5-092.DifferenceDate/src/MyDate.java
@@ -0,0 +1,40 @@
+public class MyDate {
+    private int day;
+    private int month;
+    private int year;
+
+    public MyDate(int day, int month, int year) {
+        this.day = day;
+        this.month = month;
+        this.year = year;
+    }
+
+    public String toString() {
+        return this.day + "." + this.month + "." + this.year;
+    }
+
+    public boolean earlier(MyDate compared) {
+        if (this.year < compared.year) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month < compared.month) {
+            return true;
+        }
+
+        if (this.year == compared.year && this.month == compared.month
+                && this.day < compared.day) {
+            return true;
+        }
+
+        return false;
+    }
+
+    public int differenceInYears(MyDate comparedDate){
+        int difference = this.year - comparedDate.year;
+        if(difference < 0){
+            difference *= -1;
+        }
+        return difference;
+    }
+}
